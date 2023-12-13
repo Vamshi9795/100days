8e3538b3dd6720420edf094324382a5298dc40f6
Vamshi9795 <chjyothendar@gmail.com>
2023-12-13 10:46:32 -0600

    minor tweeks

--- day1.py
+++ day1.py
@@ -1,9 +1,9 @@
 #  Creating a small brand name generator
-print( " Creating a brand name generator for friends and family") ## using print to rprint
-a = input("Enter the birthplace or your favorite city : ")
+print( "Creating a brand name generator for friends and family") ## using print to rprint
+a = input("Enter the birthplace or your favorite city : \n ")
     # taking input from the user to enter a string
-b = input("Enter your date of birth : ")
-print(f"The name of you band is {a+b} \n Thanks for using the band name geneator") ## using "F" can be helpfull to reduce the number of lines of code to b written
+b = input("Enter your date of birth : \n ")
+print(f"The name of you band is {a+b} \nThanks for using the band name geneator") ## using "F" can be helpfull to reduce the number of lines of code to b written
 
 
 
