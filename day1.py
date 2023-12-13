e7d32c1c3c2a00888284efcc7e41e26429e898fc
Vamshi9795 <chjyothendar@gmail.com>
2023-12-13 10:37:21 -0600

    added try and except block to catch any erroes

--- day1.py
+++ day1.py
@@ -1,7 +1,10 @@
 #  Creating a small brand name generator
 print( " Creating a brand name generator for friends and family") ## using print to rprint
-a = input(" Enter the hometown or city you are born in ")
-# taking input from the user to enter a string
-b = int(input( "Enter your date of birth enter just date"))
+try:
+    a = input("Enter the hometown or city you are born in ")
+    # taking input from the user to enter a string
+    b = int(input("Enter your date of birth enter just date "))
+except:
+    print( "enter a valid input in integer numbers" )
 
 
