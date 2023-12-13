fc02f71a81620d69bba493e6fd74e27933245eae
Vamshi9795 <chjyothendar@gmail.com>
2023-12-13 10:32:14 -0600

    added inut statemnts

--- day1.py
+++ day1.py
@@ -1,1 +1,7 @@
-print( " Creating a brand name generator for friends and family")
+#  Creating a small brand name generator
+print( " Creating a brand name generator for friends and family") ## using print to rprint
+a = input(" Enter the hometown or city you are born in ")
+# taking input from the user to enter a string
+b = int(input( "Enter your date of birth enter just date"))
+
+
