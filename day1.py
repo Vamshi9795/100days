7d6e04f1450c19e24a6dedd9c886f48c5403bb6e
Vamshi9795 <chjyothendar@gmail.com>
2023-12-13 10:43:57 -0600

    removed try and except cuasee we cannot concatnate int and string

--- day1.py
+++ day1.py
@@ -1,10 +1,10 @@
 #  Creating a small brand name generator
 print( " Creating a brand name generator for friends and family") ## using print to rprint
-try:
-    a = input("Enter the hometown or city you are born in ")
+a = input("Enter the birthplace or your favorite city : ")
     # taking input from the user to enter a string
-    b = int(input("Enter your date of birth enter just date "))
-except:
-    print( "enter a valid input in integer numbers" )
+b = input("Enter your date of birth : ")
+print(f"The name of you band is {a+b} \n Thanks for using the band name geneator") ## using "F" can be helpfull to reduce the number of lines of code to b written
+
+
 
 
