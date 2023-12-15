280be72d37b586871524f0724333e46100bac45f
Vamshi9795 <chjyothendar@gmail.com>
2023-12-14 23:04:11 -0600

    added zero divison error1

--- day2.py
+++ day2.py
@@ -1,7 +1,7 @@
 # building a small tip calculator that asks the total bill how many people will tip and what percentage of tip would you like to include and calculates the earch persons share
 # first we are sking the user whats the total bill and taking input from the user using INT input fuction
 try:
-    total_bill = float(input(" Enter the total bill thats on the receipt : $ \n "))
+    total_bill = float(input(" Enter the total bill thats on the receipt :\n "))
     total_people = float(input(" Enter the numer of people in the group :\n ")) #no of people in the group
     total_tip = float(input(" What percentage would you like to tip :\n ")) # total tip that are paying
     total_bill_share = (total_bill + (total_bill*total_tip)/100 )/total_people #logic cacucating tip plus ading it to the total plus didving itby total people 
