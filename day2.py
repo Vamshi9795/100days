041d4424f5de89e2ca43cfe1bf85a67f0df372a2
Vamshi9795 <chjyothendar@gmail.com>
2023-12-14 20:17:00 -0600

    added first totalbill input

---
+++ day2.py
@@ -0,0 +1,3 @@
+# building a small tip calculator that asks the total bill how many people will tip and what percentage of tip would you like to include and calculates the earch persons share
+# first we are sking the user whats the total bill and taking input from the user using INT input fuction
+total_bill = int(input(" Enter the total bill thats on the receipt : "))
