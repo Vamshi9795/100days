1ac2382ae9d6db89559f3a63be80edc00da756a4
Vamshi9795 <chjyothendar@gmail.com>
2023-12-14 20:19:10 -0600

    added total poele and changed total bill to float

--- day2.py
+++ day2.py
@@ -1,3 +1,4 @@
 # building a small tip calculator that asks the total bill how many people will tip and what percentage of tip would you like to include and calculates the earch persons share
 # first we are sking the user whats the total bill and taking input from the user using INT input fuction
-total_bill = int(input(" Enter the total bill thats on the receipt : "))
+total_bill = float(input(" Enter the total bill thats on the receipt : "))
+total_people = int(input(" Enter the numer of people in the group : "))
