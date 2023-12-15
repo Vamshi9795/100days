53e6e7c0d5fcc96fe32892234654e846579da787
Vamshi9795 <chjyothendar@gmail.com>
2023-12-14 20:20:35 -0600

    added tip percentage

--- day2.py
+++ day2.py
@@ -1,4 +1,5 @@
 # building a small tip calculator that asks the total bill how many people will tip and what percentage of tip would you like to include and calculates the earch persons share
 # first we are sking the user whats the total bill and taking input from the user using INT input fuction
-total_bill = float(input(" Enter the total bill thats on the receipt : "))
-total_people = int(input(" Enter the numer of people in the group : "))
+total_bill = float(input(" Enter the total bill thats on the receipt :\n "))
+total_people = int(input(" Enter the numer of people in the group :\n "))
+total_tip = float(input(" What percentage would you like to tip :\n "))
