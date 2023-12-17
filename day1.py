aaed053d4fc11ce3dada5042dde62284c43f8d9e
Vamshi9795 <chjyothendar@gmail.com>
2023-12-17 11:28:59 -0600

    nothing new

--- day1.py
+++ day1.py
@@ -1,11 +1,7 @@
-#  Creating a small brand name generator
-print( "Creating a brand name generator for friends and family") ## using print to rprint
-a = input("Enter the birthplace or your favorite city : \n ")
-    # taking input from the user to enter a string
-b = input("Enter your date of birth : \n ")
-print(f"The name of you band is {a+b} \nThanks for using the band name geneator") ## using "F" can be helpfull to reduce the number of lines of code to b written
-
-
-
-
-
+year = 1900
+if year % 4 == 0:   
+  print("Leap year")
+elif year % 100 == 0 and year % 400 == 0: 
+    print("Leap year") 
+else:
+  print("Not leap year")
